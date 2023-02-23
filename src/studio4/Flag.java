124557476dd336e0a11b52c6046869d0b09ccc2a
raymo <raymo@Verstappen.dhcp.wustl.edu>
2023-02-23 14:13:46 -0600

    sasdsad

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,15 @@
 package studio4;
 
 import java.awt.Color;
+import java.util.Scanner;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
 		
+		//Scanner scan = new Scanner(System.in);
+		StdDraw.setPenColor(Color.PINK);
+		StdDraw.filledRectangle(10, 10, 10, 10);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,39 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		int amountPoints = 6;
+		//Shape Name
+		String name = in.next();
 		
+		int red =in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		
+		boolean filled = in.nextBoolean();
+	
+		double one=in.nextDouble();
+		double two=in.nextDouble();
+		double three=in.nextDouble();
+		double four=in.nextDouble();
+		double five=in.nextDouble();
+		double six=in.nextDouble();
+		double[] x = { one, two, three};
+		double[] y = { four, five, six};
+		
+		if(name .equals("rectangle"))
+		{
+			StdDraw.setPenColor(red,green,blue);
+			StdDraw.rectangle(one, two, three, four);
+		}
+		if(name .equals("ellipse"))
+		{
+			StdDraw.setPenColor(red,green,blue);
+			StdDraw.ellipse(one, two, three, four);
+		}
+		if(name .equals("triangle"))
+		{
+			StdDraw.setPenColor(black);
+			StdDraw.polygon(x,y);
+		}
 	}
 }
